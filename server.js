56c281de4e18cba8560df3565fc56c6a5a34478e
rephcongmon <rephcongmon@gmail.com>
2019-07-10 22:33:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.use(express.static(__dirname + '/public/'));
+
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
 });

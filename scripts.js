0a36c782cd4e7ea769933b02cff0b2f0d0343b94
rephcongmon <rephcongmon@gmail.com>
2019-07-10 22:08:27 -0400

    Added TOKEN

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const bh = require('brawlhalla-api')('XOQ64KYMCXKEANE7JCAQYS10CFYNS');
 
 var x = "ruprep444";
+var TOKEN = process.env.brawlhallaKEY;
 
 bh.getSteamId(`https://steamcommunity.com/id/${x}/`)
     .then(function(steamID){

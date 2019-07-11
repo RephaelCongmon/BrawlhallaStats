46749c6df9f46ddf0b25ea9ed2e99a1ffdbfae53
rephcongmon <rephcongmon@gmail.com>
2019-07-11 17:57:01 -0400

    Added public folder

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public'))
 
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');

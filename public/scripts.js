0ad4d5843fd46991d3beff34b87c5723ed5e731b
rephcongmon <rephcongmon@gmail.com>
2019-07-12 00:39:26 -0400

    removed scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,36 +1,36 @@
-const bh = require('brawlhalla-api')('XOQ64KYMCXKEANE7JCAQYS10CFYNS');
-const fetch = require('node-fetch');
+// const bh = require('brawlhalla-api')('XOQ64KYMCXKEANE7JCAQYS10CFYNS');
+// const fetch = require('node-fetch');
 
-var x = "ruprep444";
-var TOKEN = process.env.brawlhallaKEY;
+// var x = "ruprep444";
+// var TOKEN = process.env.brawlhallaKEY;
 
-bh.getSteamId(`https://steamcommunity.com/id/${x}/`)
-    .then(function(steamID){
-        console.log("steamID = ", steamID);
-    }).catch(function(error){
-        console.log("Error: ", error);
-    });
+// bh.getSteamId(`https://steamcommunity.com/id/${x}/`)
+//     .then(function(steamID){
+//         console.log("steamID = ", steamID);
+//     }).catch(function(error){
+//         console.log("Error: ", error);
+//     });
 
 
-function foo(){
-    var name = document.formxml.player.value;
-    alert("Your name is " + name);
-}
+// function foo(){
+//     var name = document.formxml.player.value;
+//     alert("Your name is " + name);
+// }
 
-function getInput(){
-    var res = "";
+// function getInput(){
+//     var res = "";
 
-    res = document.formxml.player.value;
-    return res;
-}
+//     res = document.formxml.player.value;
+//     return res;
+// }
 
-function clickButton(){
-    console.log("Running clickButton");
-    var x = getInput();
-    console.log("x = " + x);
+// function clickButton(){
+//     console.log("Running clickButton");
+//     var x = getInput();
+//     console.log("x = " + x);
 
-    fetch(`https://api.brawlhalla.com/player/${x}/stats?api_key=${TOKEN}`)
-        .then(res => res.json())
-        .then(json => console.log(json));
+//     fetch(`https://api.brawlhalla.com/player/${x}/stats?api_key=${TOKEN}`)
+//         .then(res => res.json())
+//         .then(json => console.log(json));
 
-}
+// }

f019a1b414666aae64c37b3c1719aefb83a99890
Rephael Congmon <rephcongmon@gmail.com>
2019-07-18 11:27:07 -0400

    testing button event

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ router.get('/leaderboards/1v1Ranked', async function(req, res) {
     //var x = req.query.player;
     console.log(req.query);
 
-    await fetch(`https://api.brawlhalla.com/player/${x}/stats?api_key=${TOKEN}`)
+    await fetch(`https://api.brawlhalla.com/rankings/1v1/all/1?api_key=${TOKEN}`)
         .then(res => res.json())
         .then(json => {
 

fd753c3790cb5e4511d97a014d8c14b574f6476b
Rephael Congmon <rephcongmon@gmail.com>
2019-07-18 13:05:31 -0400

    using keys[0]

--- server.js
+++ server.js
@@ -57,8 +57,13 @@ router.get('/submit-form', async function(req, res) {
 router.get('/leaderboards/1v1Ranked', async function(req, res) {
     console.log("1v1 Button clicked submitted");
 
-    
-    console.log(`req.query['1v1'] = ${req.query['1v1']}`);
+    var keys = [];
+
+    for (var key in req.query) {
+        if (req.query.hasOwnProperty(key)) {
+            keys.push(key);
+        }
+    console.log(`keys[0] = ${keys[0]}`);
 
     await fetch(`https://api.brawlhalla.com/rankings/1v1/all/1?api_key=${TOKEN}`)
         .then(res => res.json())

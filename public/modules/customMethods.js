88b40a9cb5d277025ca224900adb0c17f0f9fd61
rephcongmon <rephcongmon@gmail.com>
2020-03-03 03:16:51 -0500

    fixed typo

--- public/modules/customMethods.js
+++ public/modules/customMethods.js
@@ -230,7 +230,7 @@ function showStats(result) {
             $.noConflict();
             $('#leaderboard-table').DataTable( {
                 colReorder: true,
-                "bSortCellsTop": true
+                "bSortCellsTop": true,
                 scrollX: true
             });
                

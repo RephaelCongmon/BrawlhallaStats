38d7f71fbf9e21f2c7b9347e593e1e0998d7f46b
rephcongmon <rephcongmon@gmail.com>
2020-04-03 01:18:35 -0400

    test

---
+++ public/assets/js/dark-mode-switch2.js
@@ -0,0 +1,43 @@
+(function() {
+    var darkSwitch = document.getElementById("darkModeClick");
+    console.log("Dark Switch loaded!");
+    if (darkSwitch) {
+      initTheme();
+      darkSwitch.addEventListener("onclick", function(event) {
+        console.log("Dark switch invoked!")
+        resetTheme();
+      });
+      function initTheme() {
+        var darkThemeSelected =
+          localStorage.getItem("darkSwitch") !== null &&
+          localStorage.getItem("darkSwitch") === "dark";
+        darkSwitch.checked = darkThemeSelected;
+        darkThemeSelected
+          ? document.body.setAttribute("data-theme", "dark") 
+          : document.body.removeAttribute("data-theme");
+  
+        if (darkThemeSelected){
+          document.getElementById("darkModeText").innerHTML=`Turn Lights On`;
+          document.getElementById("darkModeText").className="zmdi zmdi-brightness-5";
+        }
+        else {
+          document.getElementById("darkModeText").innerHTML=`Turn Lights Off`;
+          document.getElementById("darkModeText").className="zmdi zmdi-brightness-3";
+        }
+      }
+      function resetTheme() {
+        if (darkSwitch.checked) {
+          document.body.setAttribute("data-theme", "dark");
+          localStorage.setItem("darkSwitch", "dark");
+          document.getElementById("darkModeText").innerHTML=`Turn Lights On`;
+          document.getElementById("darkModeText").className="zmdi zmdi-brightness-5";
+        } else {
+          document.body.removeAttribute("data-theme");
+          localStorage.removeItem("darkSwitch");
+          document.getElementById("darkModeText").innerHTML=`Turn Lights Off`;
+          document.getElementById("darkModeText").className="zmdi zmdi-brightness-3";
+        }
+      }
+    }
+  })();
+  

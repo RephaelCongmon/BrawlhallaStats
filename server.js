8c1b3c11ac0697170b2cc9b995fa3b35a8a2b17b
rephcongmon <rephcongmon@gmail.com>
2020-04-16 15:55:52 -0400

    test

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ async function showStatsByID(id, req, res){
         jsonRanked = json;
     });
 
-    if (!jsonRanked.name){
+    if (jsonRanked == undefined){
         jsonRanked.error.code = 404;
         console.log("jsonRanked is not found.\njsonRanked: ", jsonRanked);
         //res.json(jsonRanked);
